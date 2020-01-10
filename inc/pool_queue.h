cbcac043dfbd0faf977e4b8fc5f8078bbcca7095
jsvirzi <jsvirzi@gmail.com>
2020-01-10 00:17:50 -0800

    use fixed sizes of array of pointers. works on one compiler but not with Qt. this is not a happy commit

--- inc/pool_queue.h
+++ inc/pool_queue.h
@@ -8,16 +8,20 @@ extern "C"
 {
 #endif
 
+#define PoolQueueBuffs (128)
+#define PoolQueueBuffLength (1024)
 typedef struct PoolQueue {
-    uint8_t **buff;
-    unsigned int *length;
+    // uint8_t **buff;
+    uint8_t buff[PoolQueueBuffs][PoolQueueBuffLength];
+    // unsigned int *length;
+    unsigned int length[PoolQueueBuffs];
     unsigned int head;
     unsigned int tail;
     unsigned int mask;
     unsigned int buff_mask;
 } PoolQueue;
 
-void initialize_pool_queue(PoolQueue *pool, int buffs, int buff_size);
+void initialize_pool_queue(PoolQueue *pool, unsigned int buffs, unsigned int buff_size);
 void release_pool_queue(PoolQueue *pool);
 
 #ifdef __cplusplus
--- src/pool_queue.c
+++ src/pool_queue.c
@@ -10,25 +10,37 @@
 
 #include "pool_queue.h"
 
-void initialize_pool_queue(PoolQueue *pool, int buffs, int buff_size) {
+void initialize_pool_queue(PoolQueue *pool, unsigned int buffs, unsigned int buff_size) {
     bzero(pool, sizeof(PoolQueue));
-    uint8_t *big_buff = (uint8_t *) malloc(buffs * buff_size);
+    pool->mask = PoolQueueBuffs - 1;
+    pool->buff_mask = PoolQueueBuffLength - 1;
+    pool->head = 0;
+    pool->tail = 0;
+#if 0
+    uint8_t *big_buff;
+    if (pool->buff) { big_buff = (uint8_t *) pool->buff; }
+    else { big_buff = (uint8_t *) malloc(buffs * buff_size); }
+    printf("big buff @ %p\n", (void *) big_buff);
     pool->buff = (uint8_t **) malloc(buffs * sizeof(uint8_t *));
     pool->mask = buffs - 1;
     pool->length = (unsigned int *) malloc(buffs * sizeof(unsigned int));
-    for (int i = 0; i < buffs; ++i) {
+    for (unsigned int i = 0; i < buffs; ++i) {
         pool->buff[i] = (uint8_t *) &big_buff[i * buff_size];
+        if (i < 128) { printf("pool pointer %d = %p. size = %d\n", i, (void *) pool->buff[i], buff_size); }
+        pool->length[i] = 0;
     }
     pool->buff_mask = buff_size - 1;
+#endif
+
 }
 
 void release_pool_queue(PoolQueue *pool) {
-    free(pool->buff[0]); /* points to big buffer */
-    free(pool->buff);
+//    free(pool->buff[0]); /* points to big buffer */
+//    free(pool->buff);
 }
 
-int stream_pool_queue(PoolQueue *pool, int fd, int max_bytes) {
-    int remaining = max_bytes, n_read;
+unsigned int stream_pool_queue(PoolQueue *pool, int fd, unsigned int max_bytes) {
+    ssize_t remaining = max_bytes, n_read;
     unsigned int buff_size = pool->buff_mask + 1;
     while (remaining > pool->mask) {
         n_read = read(fd, pool->buff[pool->head], buff_size);
@@ -36,18 +48,18 @@ int stream_pool_queue(PoolQueue *pool, int fd, int max_bytes) {
             remaining = remaining - n_read;
             unsigned int new_head = (pool->head + 1) & pool->mask;
             if (new_head != pool->tail) { pool->head = new_head; }
-            else { return max_bytes - remaining; }
+            else { return (unsigned int) (max_bytes - remaining); }
         }
     }
     if (remaining) {
-        n_read = read(fd, pool->buff[pool->head], remaining);
+        n_read = read(fd, pool->buff[pool->head], (size_t) remaining);
         if (n_read > 0) {
             pool->head = (pool->head + 1) & pool->mask;
             remaining = remaining - n_read;
             unsigned int new_head = (pool->head + 1) & pool->mask;
             if (new_head != pool->tail) { pool->head = new_head; }
-            else { return max_bytes - remaining; }
+            else { return (unsigned int) (max_bytes - remaining); }
         }
     }
-    return (max_bytes - remaining);
+    return (unsigned int) (max_bytes - remaining);
 }
